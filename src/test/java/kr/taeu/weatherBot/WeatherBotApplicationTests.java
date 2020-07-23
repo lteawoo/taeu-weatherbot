126c0f89169c1221ddb209cd22968d46275d212b
twlee <lteawoo@naver.com>
2020-07-23 19:12:43 +0900

    init

---
+++ src/test/java/kr/taeu/weatherBot/WeatherBotApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.taeu.weatherBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
